4f35324c941b66a5ba318ac508b7889d0d297d65
Bao Son <baoson20022001@gmail.com>
2025-06-15 20:24:15 +0700

    delete function

--- code/auth/auth_handler.py
+++ code/auth/auth_handler.py
@@ -80,8 +80,3 @@ async def get_current_active_user(current_user: UserInDB = Depends(get_current_u
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
-async def get_current_active_user2(current_user: UserInDB = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
-
